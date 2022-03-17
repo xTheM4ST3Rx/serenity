6395a6c7044b2707d541a678c8bb7125a1764a92
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-03-17 10:58:13 -0300

    update changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import Logo from "../resources/SerenityDesign.png";
 export function Sidebar(){
    return(<>
         <div id="SideBar">
-            <div className="nav-link"><img src={Logo} alt="Logo"width={"50px"}/></div>
+            <div className="nav-link align-self-center"><img src={Logo} alt="Logo"width={"50px"}/> SerenityDesgin</div>
             <div className="nav-link"><Link to="/">Portfolio</Link></div>
             <div className="nav-link"><Link to="/layout">Layout</Link></div>
         </div>
