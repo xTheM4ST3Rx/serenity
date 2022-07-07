b897ce03e499e19e01262e47373c9350aa79a630
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-07-07 15:43:09 -0300

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Serenity/",
-  server: {
-    host: true,
-    port: 3000,
-  },
-  preview: {
-    host: true,
-    port: 3001,
-  },
   build: {
     sourcemap: true,
   },
