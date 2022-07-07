dfcfa6f09473bf8685cd56f9ebf90ba2225140d3
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-07-07 16:26:04 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Serenity/",
-  build: {
-    sourcemap: true,
-  },
 });
 
 //Default port 4173
+//git subtree push --prefix dist github gh-pages
