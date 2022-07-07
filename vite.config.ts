da76fb2bce8c05f804cefd1d54cf409b8c30b244
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-07-07 17:43:32 -0300

    update vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/serenity/",
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 });
 
 //Default port 4173
