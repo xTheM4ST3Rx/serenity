5d37bbc0426231ed5383616b0d1e9f87220b45fb
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-07-07 14:57:44 -0300

    update github

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Serenity/",
-  server: {
-    port: 5001,
-  },
-  preview: {
-    port: 5002,
-  },
+  base: "/serenity/",
 });
