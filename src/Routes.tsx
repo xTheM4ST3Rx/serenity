c02b850a51dead49a451a585e94521985431e17a
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-07-08 10:28:16 -0300

    update

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
-import Teste from "./pages/Teste";
 
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/serenity" element={<Dashboard />} />
-        <Route path="/serenity/teste" element={<Teste />} />
       </Routes>
     </Router>
   );
